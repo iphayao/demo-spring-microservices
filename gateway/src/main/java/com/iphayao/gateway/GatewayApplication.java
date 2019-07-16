bb827a77da8d0bde9aeeabdfe8fd42d8bfc2d618
iphayao <phayaob@gmail.com>
2019-07-16 20:05:25 +0700

    Add API Gateway with Zuul

--- gateway/src/main/java/com/iphayao/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/iphayao/gateway/GatewayApplication.java
@@ -1,15 +1,15 @@
-package com.iphayao.service;
+package com.iphayao.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
-public class OrganizationApplication {
-
+public class GatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(OrganizationApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
-
 }
